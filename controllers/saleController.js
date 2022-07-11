2a599f359ab3d15216c0686d0d563670501368a3
GabsRRC <gabriellerrc@gmail.com>
2022-07-11 00:55:12 -0300

    added: sale route

--- controllers/saleController.js
+++ controllers/saleController.js
@@ -1,12 +1,12 @@
 import { db, objectId } from "../database/mongoDB.js";
 
-async function renderProducts(req, res, next) {
+async function renderSale(req, res, next) {
   try {
-    const products = await db.collection("products").find().toArray();
+    const products = await db.collection("sale").find().toArray();
     return res.send(products);
   } catch (e) {
     return res.sendStatus(500);
   }
 }
 
-export default renderProducts;
+export default renderSale;
--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,9 +1,13 @@
 import renderProducts from "../controllers/productsController.js";
+import renderSale from "../controllers/saleController.js";
 import { Router } from "express";
 
 const router = Router();
 
-//Renderizar mensagens
+//Renderizar produtos
 router.get("/products", renderProducts);
+
+//Renderiza promoções
+router.get("/sale" , renderSale)
 
 export default router;
